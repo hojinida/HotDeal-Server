dfcb342a78a680c193c6e269990e20f2bebba517
Hojinida <opqrs1103@gmail.com>
2023-03-06 15:12:05 +0900

    feat: Login Api, Auth Controller

--- src/main/java/com/personal/hotdeal/user/presentation/MemberController.java
+++ src/main/java/com/personal/hotdeal/user/presentation/MemberController.java
@@ -6,17 +6,23 @@ import com.personal.hotdeal.user.presentation.dto.JoinRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController("member")
 @RequiredArgsConstructor
 public class MemberController {
     private final MemberService memberService;
     @PostMapping("/join")
     public ResponseEntity<Void> join(@RequestBody @Validated JoinRequestDto joinRequestDto){
         memberService.join(joinRequestDto);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> delete(){
         return ResponseEntity.ok().build();
     }
 }
