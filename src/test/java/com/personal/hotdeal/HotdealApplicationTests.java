63f67c72d83453afea295130d56122c96167041b
Hojinida <opqrs1103@gmail.com>
2023-03-27 16:33:33 +0900

    test: ci/cd

--- src/test/java/com/personal/hotdeal/HotdealApplicationTests.java
+++ src/test/java/com/personal/hotdeal/HotdealApplicationTests.java
@@ -3,6 +3,7 @@ package com.personal.hotdeal;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+ㅎ
 @SpringBootTest
 class HotdealApplicationTests {
 
